d3d5a38cfb0c47cfe2c85479f4218a326e7805cf
Ian Langworth <ian@langworth.com>
2009-12-02 04:48:18 -0800

    install.sh now handles ~/bin

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@
 #
 
 basedir=$HOME/.dotfiles
+bindir=$HOME/bin
 gitbase=git://github.com/statico/dotfiles.git
 tarball=http://github.com/statico/dotfiles/tarball/master
 
@@ -30,6 +31,23 @@ function die() {
     exit 1
 }
 
+function install() {
+    src=$1
+    dest=$2
+    if [ -e $dest && ! -s $dest ]; then
+        # Rename files with a ".old" extension.
+        warn "$dest file already exists, renaming to $dest.old"
+        backup=$dest.old
+        if [ -e $backup ]; then
+            die "$backup already exists. Aborting."
+        fi
+        mv -v $dest $backup
+    fi
+
+    # Update existing or create new symlinks.
+    ln -v -s -f $src $dest
+}
+
 mkdir -p $basedir
 cd $basedir
 
@@ -37,8 +55,10 @@ if [ ! -e $basedir ]; then
     # .dotfiles directory needs to be installed. Try downloading first with
     # git, then use tarballs.
     if has git; then
+        note "Cloning from git..."
         git clone $gitbase $basedir
     else
+        note "Downloading tarball..."
         tempfile=TEMP.tar.gz
         if has curl; then
             curl $tarball >$tempfile
@@ -52,26 +72,17 @@ if [ ! -e $basedir ]; then
     fi
 fi
 
-# Symlink all files in place.
-for path in $( find . ); do
-    if [ -d $path ]; then
-        mkdir -v -p "$HOME/$( dirname $path )"
-    else
-        src=$basedir/$path
-        dest=$HOME/$path
+# Symlink all dotfiles.
+note "Installing dotfiles..."
+for path in .*; do
+    install $basedir/$path $HOME/$path
+done
 
-        if [ -f $dest ]; then
-            # Rename files with a ".old" extension.
-            warn "$dest file already exists, renaming to $dest.old"
-            backup=$dest.old
-            if [ -e $backup ]; then
-                die "$backup already exists. Aborting."
-            fi
-            mv -v $dest $backup
-        fi
-
-        ln -s -f $src $dest
-    fi
+# Install any utilities into ~/bin.
+note "Installing bin/ directory..."
+mkdir -v -p $bindir
+for path in ./bin/*; do
+    install $basedir/$path $bindir/$path
 done
 
 note "Done."
