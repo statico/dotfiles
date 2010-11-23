8515f25e227af5211c15b6fff72e0ad035dca9b6
Ian Langworth <ian@redbeacon.com>
2010-11-23 13:30:15 -0500

    init.el tweaks

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -52,6 +52,10 @@
  indent-tabs-mode nil
  iswitchb-mode t
  auto-fill-mode t
+ tab-width 2
+ py-indent-offset 4
+ css-indent-offset 2
+ javascript-indent-offset 2
  case-fold-search t)
 
 (transient-mark-mode 1)
@@ -80,6 +84,25 @@
 (ac-config-default)
 
 (load "~/.emacs.d/nxhtml/autostart.el")
+(setq mumamo-background-colors nil) 
+
+(add-to-list 'auto-mode-alist '("\\.html$" . django-html-mumamo-mode))
+
+;; source: http://steve.yegge.googlepages.com/my-dot-emacs-file
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file name new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Python
