41f5641962443b44a8107df89795c13c71029cee
Ian Langworth ☠ <ian@langworth.com>
2011-01-22 12:01:46 -0800

    Copied window-moving keys from rmm5t

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -349,6 +349,28 @@
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings))
 
+;; Map the window manipulation keys to meta 0, 1, 2, o (from rmm5t)
+(global-set-key (kbd "M-3") 'split-window-horizontally) ; was digit-argument
+(global-set-key (kbd "M-2") 'split-window-vertically) ; was digit-argument
+(global-set-key (kbd "M-1") 'delete-other-windows) ; was digit-argument
+(global-set-key (kbd "M-0") 'delete-window) ; was digit-argument
+(global-set-key (kbd "M-o") 'other-window) ; was facemenu-keymap
+;; Replace dired's M-o
+(add-hook 'dired-mode-hook
+          (lambda () (define-key dired-mode-map (kbd "M-o") 'other-window))) ; was dired-omit-mode
+;; Replace ibuffer's M-o
+(add-hook 'ibuffer-mode-hook
+          (lambda () (define-key ibuffer-mode-map (kbd "M-o") 'other-window))) ; was ibuffer-visit-buffer-1-window
+;; To help Unlearn C-x 0, 1, 2, o
+(global-unset-key (kbd "C-x 3")) ; was split-window-horizontally
+(global-unset-key (kbd "C-x 2")) ; was split-window-vertically
+(global-unset-key (kbd "C-x 1")) ; was delete-other-windows
+(global-unset-key (kbd "C-x 0")) ; was delete-window
+(global-unset-key (kbd "C-x o")) ; was other-window
+
+;; Easier buffer killing
+(global-set-key (kbd "M-k") 'kill-this-buffer)
+
 ;; Modern IDE-like line highlighting (subtle)
 (require 'highlight-current-line)
 (setq highlight-current-line-globally t)
