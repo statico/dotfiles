6c4a4048d225944bb9ebefe8db6de9f80e3c9cba
Ian Langworth ☠ <ian@langworth.com>
2013-03-02 22:23:57 -0500

    vim-css-color is too slow

--- .vim/update.sh
+++ .vim/update.sh
@@ -38,7 +38,6 @@ repos=(
   https://github.com/Lokaltog/vim-easymotion.git
   https://github.com/Lokaltog/vim-powerline.git
   https://github.com/altercation/vim-colors-solarized.git
-  https://github.com/skammer/vim-css-color.git
   https://github.com/docunext/closetag.vim.git
   https://github.com/hced/bufkill-vim.git
   https://github.com/kchmck/vim-coffee-script.git
