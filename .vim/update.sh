ed89c4aa3fa1efde14faa899a8d83faadb334e08
Ian Langworth <ianl@looker.com>
2017-06-27 13:34:48 -0700

    Big update script cleanup

--- .vim/update.sh
+++ .vim/update.sh
@@ -6,13 +6,9 @@
 #
 #   ./update.sh
 #
-# Update just the things from Git:
+# Update just one plugin that matches this pattern:
 #
-#   ./update.sh repos
-#
-# Update just one plugin from the list of Git repos:
-#
-#   ./update.sh repos supertab
+#   ./update.sh supertab
 #
 
 cd ~/.dotfiles
@@ -22,139 +18,48 @@ bundledir=$vimdir/bundle
 tmp=/tmp/$LOGNAME-vim-update
 me=.vim/update.sh
 
-# I have an old server with outdated CA certs.
-if [ -n "$INSECURE" ]; then
-  curl='curl --insecure'
-  export GIT_SSL_NO_VERIFY=true
-else
-  curl='curl'
-fi
-
-# URLS --------------------------------------------------------------------
-
-# This is a list of all plugins which are available via Git repos. git:// URLs
-# don't work.
 repos=(
 
-  https://github.com/airblade/vim-gitgutter.git
-  https://github.com/altercation/vim-colors-solarized.git
-  https://github.com/ap/vim-css-color.git
-  https://github.com/ctrlpvim/ctrlp.vim.git
-  https://github.com/docunext/closetag.vim.git
+  https://github.com/airblade/vim-gitgutter
+  https://github.com/altercation/vim-colors-solarized
+  https://github.com/ap/vim-css-color
+  https://github.com/ctrlpvim/ctrlp.vim
+  https://github.com/docunext/closetag.vim
   https://github.com/ervandew/supertab
   https://github.com/haya14busa/incsearch.vim
-  https://github.com/junegunn/goyo.vim.git
+  https://github.com/junegunn/goyo.vim
   https://github.com/justinmk/vim-sneak
-  https://github.com/rking/ag.vim.git
-  https://github.com/scrooloose/nerdtree.git
-  https://github.com/scrooloose/syntastic.git
-  https://github.com/sheerun/vim-polyglot.git
-  https://github.com/tomasr/molokai.git
+  https://github.com/rking/ag.vim
+  https://github.com/scrooloose/nerdtree
+  https://github.com/scrooloose/syntastic
+  https://github.com/sheerun/vim-polyglot
+  https://github.com/tomasr/molokai
   https://github.com/tpope/vim-commentary
-  https://github.com/tpope/vim-fugitive.git
-  https://github.com/tpope/vim-markdown.git
-  https://github.com/tpope/vim-pathogen.git
-  https://github.com/tpope/vim-sleuth.git
-  https://github.com/tpope/vim-surround.git
+  https://github.com/tpope/vim-fugitive
+  https://github.com/tpope/vim-markdown
+  https://github.com/tpope/vim-pathogen
+  https://github.com/tpope/vim-sleuth
+  https://github.com/tpope/vim-surround
   https://github.com/tpope/vim-unimpaired
   https://github.com/vim-airline/vim-airline
   https://github.com/vim-airline/vim-airline-themes
-  https://github.com/vim-scripts/bufkill.vim.git
-  https://github.com/wellle/targets.vim.git
+  https://github.com/vim-scripts/bufkill.vim
+  https://github.com/wellle/targets.vim
   https://github.com/yssl/QFEnter
 
 )
 
-# Here's a list of everything else to download in the format
-# <destination>;<url>[;<filename>]
-other=(
-  'glsl/syntax;http://www.vim.org/scripts/download_script.php?src_id=3194;glsl.vim'
-)
+mkdir -p $bundledir
 
-case "$1" in
-
-  # GIT -----------------------------------------------------------------
-  repos|repo)
-    mkdir -p $bundledir
-    for url in ${repos[@]}; do
-      if [ -n "$2" ]; then
-        if ! (echo "$url" | grep "$2" &>/dev/null) ; then
-          continue
-        fi
-      fi
-      dest="$bundledir/$(basename $url | sed -e 's/\.git$//')"
-      rm -rf $dest
-      echo "Cloning $url"
-      git clone --depth=1 -q $url $dest
-      rm -rf $dest/.git
-    done
-    ;;
-
-  # TARBALLS AND SINGLE FILES -------------------------------------------
-  other)
-    set -x
-    mkdir -p $bundledir
-    rm -rf $tmp
-    mkdir $tmp
-    pushd $tmp
-
-    for pair in ${other[@]}; do
-      parts=($(echo $pair | tr ';' '\n'))
-      name=${parts[0]}
-      url=${parts[1]}
-      filename=${parts[2]}
-      dest=$bundledir/$name
-
-      rm -rf $dest
-
-      if echo $url | egrep '.zip$'; then
-        # Zip archives from VCS tend to have an annoying outer wrapper
-        # directory, so unpacking them into their own directory first makes it
-        # easy to remove the wrapper.
-        f=download.zip
-        $curl -sL $url >$f
-        unzip $f -d $name
-        mkdir -p $dest
-        mv $name/*/* $dest
-        rm -rf $name $f
-
-      else
-        # Assume single files. Create the destination directory and download
-        # the file there.
-        mkdir -p $dest
-        pushd $dest
-        if [ -n "$filename" ]; then
-          $curl -sL $url >$filename
-        else
-          $curl -sOL $url
-        fi
-        popd
-
-      fi
-
-    done
-
-    popd
-    rm -rf $tmp
-    ;;
-
-  # HELP ----------------------------------------------------------------
-
-  all)
-    $me repos
-    $me other
-    echo
-    echo "Update OK"
-    ;;
-
-  *)
-    set +x
-    echo
-    echo "Usage: $0 <section> [<filter>]"
-    echo "...where section is one of:"
-    grep -E '\w\)$' $me | sed -e 's/)//'
-    echo
-    echo "<filter> can be used with the 'repos' section."
-    exit 1
-
-esac
+for url in ${repos[@]}; do
+  if [ -n "$1" ]; then
+    if ! (echo "$url" | grep -i "$1" &>/dev/null) ; then
+      continue
+    fi
+  fi
+  dest="$bundledir/$(basename $url | sed -e 's/\.git$//')"
+  rm -rf $dest
+  echo "Cloning $url"
+  git clone --depth=1 -q $url $dest
+  rm -rf $dest/.git
+done
