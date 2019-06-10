66d381a74c54e8782358777e28b8f39d82733307
Ian Langworth ☠ <ian@langworth.com>
2019-06-09 19:53:13 -0700

    Check out zsh-completions quietly

--- install.sh
+++ install.sh
@@ -53,7 +53,7 @@ cd "$basedir"
 
 echo "Updating common Zsh completions..."
 rm -rf .zsh-completions ~/.zcompdump
-git clone --depth=1 https://github.com/zsh-users/zsh-completions .zsh-completions
+git clone --quiet --depth=1 https://github.com/zsh-users/zsh-completions .zsh-completions
 
 echo "Creating symlinks..."
 for path in .* ; do
