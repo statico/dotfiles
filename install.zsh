07a15b6ba1a050e283086d6d42303efd3bb9a132
Ian Langworth <ian@langworth.com>
2022-04-01 12:57:49 -0700

    Update repo URL in install.zsh

--- install.zsh
+++ install.zsh
@@ -5,7 +5,7 @@ set -eo pipefail
 
 basedir="$HOME/.dotfiles"
 bindir="$HOME/bin"
-repourl="git://github.com/statico/dotfiles.git"
+repourl="https://github.com/statico/dotfiles.git"
 
 function symlink() {
   src="$1"
