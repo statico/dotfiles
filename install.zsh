83161d528c50f175d17f8ab0c0a5a297ccb21cf2
Ian Langworth <ian@fluxon.com>
2022-08-30 13:56:22 -0700

    Update VS Code git mergetool setup

--- install.zsh
+++ install.zsh
@@ -111,10 +111,10 @@ if which git-lfs >/dev/null 2>&1 ; then
 fi
 if which code >/dev/null 2>&1 ; then
   echo "VS Code found. Configuring Git to use it."
+  git config --global merge.tool vscode
+  git config --global mergetool.vscode.cmd 'code --wait --merge $REMOTE $LOCAL $BASE $MERGED'
   git config --global diff.tool vscode
   git config --global difftool.vscode.cmd 'code --wait --diff $LOCAL $REMOTE'
-  git config --global merge.tool vscode
-  git config --global mergetool.vscode.cmd 'code --wait $MERGED'
 fi
 
 if which tmux >/dev/null 2>&1 ; then
